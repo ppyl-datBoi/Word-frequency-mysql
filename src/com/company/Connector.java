1058ce0239b1591cf3ebb42733f4eea03e079e58
cfb <dfb>
2020-06-02 23:24:30 +0500

    Task3 - bad verse but work edition

---
+++ src/com/company/Connector.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Connector {
+    public static void executeQuery (String query){
+        try{
+            //String url = "jdbc:mysql://www.db4free.net:3306/freq_admin";
+            //String username = "freq_admin";
+            //String password = "freq_admin";
+            String url = "jdbc:mysql://localhost:3306?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+            String password = "11111111";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+                System.out.println("Connection to Store DB succesfull!");
+                Statement statement = conn.createStatement();
+                System.out.println(query);
+                statement.executeUpdate(query);
+
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+
+    }
+    public static Map getDataFromBD (String query){
+        Map<Integer, String> result = new HashMap<>();;
+        try{
+            String url = "jdbc:mysql://localhost:3306?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+            String password = "11111111";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+                System.out.println("Connection to Store DB succesfull!");
+                Statement statement = conn.createStatement();
+                ResultSet rs = statement.executeQuery(query);
+                while(rs.next()) {
+                    result.put(rs.getInt("id"), rs.getString("path"));
+                    String filesPath = rs.getString("path");
+                    System.out.println(filesPath);
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+        return result;
+    }
+}
---
+++ src/com/company/TextFile.java
@@ -0,0 +1,120 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.*;
+
+public class TextFile extends Thread {
+    Integer index;
+    String path;
+    Map<String, Integer> wordMap;
+    TextFile(Integer index, String path){
+        this.index = index;
+        this.path = path;
+        wordMap = new HashMap<>(); // создаем свой map
+    }
+    public static void executeQuery (String query){
+        try{
+
+            String url = "jdbc:mysql://localhost:3306?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+            String password = "11111111";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+                System.out.println("Connection to Store DB succesfull!");
+                Statement statement = conn.createStatement();
+                System.out.println(query);
+                statement.executeUpdate(query);
+
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+
+    }
+    public static void printFrequency(Map wordMap) {
+
+        // Создание связанного списка для хранения всего набора записей
+        List<Map.Entry<String, Integer>> linkedMap = new LinkedList<>(wordMap.entrySet());
+
+        // сортировка
+        Collections.sort(linkedMap, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return -1 * o1.getValue().compareTo(o2.getValue());
+            }
+        });
+
+        int num = 1;
+        //System.out.printf("%-5s %-10s %-10s\n", "№", "Слово", "Частота");
+        //System.out.printf("%-30s\n", "----------------------------");
+        for (Map.Entry<String, Integer> wordFreq : linkedMap) {
+            //System.out.printf("%-5d %-15s %-5d\n", num, wordFreq.getKey(), wordFreq.getValue());
+            num++;
+        }
+    }
+    public static Map GetStatistics(String fileName) {
+        Map<String, Integer> wordMap = new HashMap<>();
+        try {
+
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+
+            // читаем пока есть строки
+            while ((line = reader.readLine()) != null) {
+                // Конвертируем к маленькому регистру
+                line = line.toLowerCase();
+                // Убираем все, кроме русских букв
+                line = line.replaceAll("[^\\sа-яА-ЯёЁ]", "");
+                // Убираем лишние проблеы
+                line = line.replaceAll("\\s+", " ").trim();
+
+                // если строка не пустая
+                if (!line.isEmpty()) {
+                    // Разбиваем линию на строки
+                    String[] s = line.split(" ");
+                    // Перебераем слова
+                    for (String word : s) {
+                        // Если слово уже существует, то увеличиваем счетчик
+                        if (wordMap.containsKey(word)) {
+                            Integer count = wordMap.get(word);
+                            wordMap.put(word, count + 1);
+                        } else {
+                            //Иначе создаем новую запись
+                            wordMap.put(word, 1);
+                        }
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordMap;
+    }
+    public void run(){
+
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        System.out.println(path);
+        wordMap = GetStatistics(path); //
+        for (Iterator<Map.Entry<String, Integer>> entries = wordMap.entrySet().iterator(); entries.hasNext(); ) {
+            Map.Entry<String, Integer> entry = entries.next();
+            //System.out.println(entry.getValue());
+            String query = "INSERT INTO `freq`.`words` (`word`, `freq`, `file_id`) VALUES ('" + entry.getKey()+ "', '" + entry.getValue()+ "', '" + index +"')";
+            executeQuery(query);
+        }
+        //printFrequency(wordMap);
+        System.out.printf("%s fiished... \n", Thread.currentThread().getName());
+    }
+}
